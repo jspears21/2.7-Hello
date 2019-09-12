5d0700a7edd40e334f55311d2df39ea8c90cbc96
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-09-11 18:25:34 -0700

    Initial Commit

---
+++ 2.7 Hello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2.7 Hello
+//
+//  Created by Jared Spears on 9/11/19.
+//  Copyright © 2019 Jared Spears. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

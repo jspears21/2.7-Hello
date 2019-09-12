4a9cf331e91f36453de9df4fa270ad6229a261b6
Jared Spears <jspears2@zagmail.gonzaga.edu>
2019-09-11 18:52:21 -0700

    hi

--- 2.7 Hello/ViewController.swift
+++ 2.7 Hello/ViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func sayHelloPressed(_ sender: UIButton) {
+        
+        let message1 = "G'Day"
+        let message2 = "Ciao"
+        let message3 = "Privyet"
+        
+        if label.text == message1 {
+            label.text = message2
+        } else if label.text == message2 {
+            label.text = message3
+        } else {
+            label.text = message1
+        }
+        
+        
+        
+        
+        
+    }
+    
 
 
 }
